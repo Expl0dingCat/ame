007a2442c2b8d0eca8529b11f5e8608cd5e7ee43
Ethan Koo <ethankoo.ca@gmail.com>
2024-05-22 21:05:01 -0400

    Lower variable name to account for CG API

--- modules/crypto_data.py
+++ modules/crypto_data.py
@@ -9,7 +9,7 @@ cg = CoinGeckoAPI()
 
 def get_info(full_crypto_name):
     try:
-        coin = cg.get_coin_by_id(id=full_crypto_name)
+        coin = cg.get_coin_by_id(id=full_crypto_name.lower())
         data = {'name': coin['name'], 'current_price (usd)': coin['market_data']['current_price']['usd'], 'market cap (usd)': coin['market_data']['market_cap']['usd'], 'total volume (usd)': coin['market_data']['total_volume']['usd'], '24h change %': coin['market_data']['price_change_percentage_24h']}
         return data
     except Exception as e:
