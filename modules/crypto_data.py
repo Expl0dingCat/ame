bfcc6238103d9dc11b80141c52f14c42e7d77347
Ethan Koo <ethankoo.ca@gmail.com>
2024-05-22 21:25:42 -0400

    Modify way variable is lowered.

--- modules/crypto_data.py
+++ modules/crypto_data.py
@@ -8,8 +8,9 @@ current_time = now.strftime("%H:%M:%S")
 cg = CoinGeckoAPI()
 
 def get_info(full_crypto_name):
+    name = full_crypto_name.lower()
     try:
-        coin = cg.get_coin_by_id(id=full_crypto_name.lower())
+        coin = cg.get_coin_by_id(id=name)
         data = {'name': coin['name'], 'current_price (usd)': coin['market_data']['current_price']['usd'], 'market cap (usd)': coin['market_data']['market_cap']['usd'], 'total volume (usd)': coin['market_data']['total_volume']['usd'], '24h change %': coin['market_data']['price_change_percentage_24h']}
         return data
     except Exception as e:
