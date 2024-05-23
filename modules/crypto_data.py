96cbccb4d618c7a54165fc8237a3d11544099af7
Ethan Koo <ethankoo.ca@gmail.com>
2024-05-22 20:59:23 -0400

    Rename variable to make detection a bit easier

--- modules/crypto_data.py
+++ modules/crypto_data.py
@@ -7,9 +7,9 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 cg = CoinGeckoAPI()
 
-def get_info(ticker):
+def get_info(full_crypto_name):
     try:
-        coin = cg.get_coin_by_id(id=ticker)
+        coin = cg.get_coin_by_id(id=full_crypto_name)
         data = {'name': coin['name'], 'current_price (usd)': coin['market_data']['current_price']['usd'], 'market cap (usd)': coin['market_data']['market_cap']['usd'], 'total volume (usd)': coin['market_data']['total_volume']['usd'], '24h change %': coin['market_data']['price_change_percentage_24h']}
         return data
     except Exception as e:
